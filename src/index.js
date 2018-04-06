7ae84fe126862d7af565a50ade0b3c49768d6458
medvedevdenis12 <medvedevdenis12@gmail.com>
2018-04-07 00:59:06 +0300

    working on connecting apollo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+import Client from 'aws-appsync';
+import { Rehydrated } from 'aws-appsync-react';
+import { ApolloProvider } from 'react-apollo';
+
+import config from './appsync';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
