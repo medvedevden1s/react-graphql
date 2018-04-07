14594aeb04420569f84ae012e404816c84e1447b
medvedevden1s <medvedevdenis12@gmail.com>
2018-04-07 12:55:52 +0300

    Connected graphql

--- src/App.js
+++ src/App.js
@@ -2,20 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {compose, graphql } from 'react-apollo';
+import ListRecipes from './queries/ListRecipes';
+
+
 class App extends Component {
   render() {
+    console.log('props: ', this.props)
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {
+          this.props.recipes.map((recipe, index) => (
+            <div>
+              <p>{recipe.name}</p>
+            </div>
+          ))
+        }
       </div>
     );
   }
 }
 
-export default App;
+export default graphql(ListRecipes, {
+  options: {
+    fetchPolicy: 'cache-and-network'
+  },
+  props: props =>({
+    recipes: props.data.listRecipes ? props.data.listRecipes.items : []
+  })
+}
+)(App);
--- src/index.js
+++ src/index.js
@@ -5,10 +5,26 @@ import App from './App';
 
 import Client from 'aws-appsync';
 import { Rehydrated } from 'aws-appsync-react';
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider as Provider} from 'react-apollo';
 
 import config from './appsync';
 
+const client = new Client({
+    url: config.graphqlEndpoint,
+    region: config.region,
+    auth: {
+        type: config.authenticationType,
+        apiKey: config.apiKey
+    }
+})
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const WithProvider = () => (
+    <Provider client={client}>
+        <Rehydrated>
+            <App />
+        </Rehydrated>
+    </Provider>
+)
+
+ReactDOM.render(<WithProvider />, document.getElementById('root'));
 
