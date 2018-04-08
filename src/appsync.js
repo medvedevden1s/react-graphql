529180b63c61d6799b76e06196fcbba9abbbb488
medvedevdenis12 <medvedevdenis12@gmail.com>
2018-04-08 21:30:33 +0300

    rm appsync

--- src/appsync.js
+++
@@ -1,6 +0,0 @@
-export default {
-    "graphqlEndpoint": "https://oile5idfb5gz7kqvcdo5lpm2te.appsync-api.us-west-2.amazonaws.com/graphql",
-    "region": "us-west-2",
-    "authenticationType": "API_KEY",
-    "apiKey": "da2-jtcaethnjvhwhithw6g3lunigq"
-}
